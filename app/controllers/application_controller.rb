9a33be5286043569f3b4224e6cc8d5f6ddb95874
Jonathan Abantao <jonabantao@gmail.com>
2018-01-28 18:09:03 -0800

    Add missing :logged_in? helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,4 +18,8 @@ class ApplicationController < ActionController::Base
     return nil unless session[:session_token]
     @current_user ||= User.find_by(session_token: session[:session_token])
   end
+
+  def logged_in?
+    !!current_user
+  end
 end
