e0d40e5a88f3c8e54778573e4afd52d69a733cf7
Jonathan Abantao <jonabantao@gmail.com>
2018-01-28 17:09:22 -0800

    Create framework for MVP ISSUE 1

---
+++ test/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user do
+    username "MyString"
+    email "MyString"
+    password_digest "MyString"
+    session_token "MyString"
+    profile_img_url "MyString"
+  end
+end
