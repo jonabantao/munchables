c34d018a4893c4504df79e6d6fcb648fd6384dfd
Jonathan Abantao <jonabantao@gmail.com>
2018-01-30 12:52:09 -0800

    Prepare for inital test Heroku push

--- frontend/components/temp.jsx
+++ frontend/components/temp.jsx
@@ -4,6 +4,7 @@ const TempPage = () => {
   return (
     <div className="test">
       <h1>This is a temp page.</h1>
+      <h2>Website to be completed the first week of February.</h2>
       <img src="https://i.imgur.com/AbQLaVE.gif" alt="temp corgi" />
     </div>
   );
