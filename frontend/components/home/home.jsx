7b2023736847578b3131d779696361685074887a
Jonathan Abantao <jonabantao@gmail.com>
2018-02-01 14:59:37 -0800

    Added more CSS rules before moving on to recipes

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <React.Fragment>
       <section className="hero-image">
-        <h1 className="hero-image__text">We need to cook.</h1>
+        <h1 className="hero-image__text">Let's make something...</h1>
       </section>
       <section className="main-container">
         <RecipeListContainer />
