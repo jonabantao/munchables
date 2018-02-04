94e6f74df345f01b9c05906ab943b74113581a2e
Jonathan Abantao <jonabantao@gmail.com>
2018-02-03 21:36:20 -0800

    Complete title form and image upload functionality

--- frontend/components/recipe/recipe_form.jsx
+++ frontend/components/recipe/recipe_form.jsx
@@ -5,48 +5,82 @@ class RecipeForm extends Component {
     super(props);
 
     this.uploadImage = this.uploadImage.bind(this);
+    this.state = {
+      imageFile: null,
+      imageUrl: null,
+    };
+  }
+
+  getInitialState() {
+    return ({
+      body: '',
+      imageFile: null,
+      imageUrl: null,
+    });
   }
 
   uploadImage(e) {
     let file = e.currentTarget.files[0];
-    const fileReader = new FileReader();
-    fileReader.onloadend = () => {
+    const reader = new FileReader();
+
+    reader.onloadend = () => {
       this.setState({
         imageFile: file,
-        imageUrl: fileReader.result
-      }).bind(this);
+        imageUrl: reader.result
+      });
     };
 
     if (file) {
-      fileReader.readAsDataURL(file);
+      reader.readAsDataURL(file);
     }
   }
 
+  update(propType) {
+    return e => this.setState({ [propType]: e.target });
+  }
+
+  displayImage() {
+    return this.state.imageUrl ? 
+      <img src={`${this.state.imageUrl}`} className="recipe-form__img-main"/> : 
+      <h3 className="recipe-form__img-upload-text">
+        <i className="fas fa-plus"></i> Click To Add Title Image
+      </h3>;
+  }
+
   render() {
+    console.log(this.state);
     return (
-      <main className="recipe-form-page">
-        <section className="recipe-form">
-          <section className="recipe-form__title-window">
-            <section
-              className="recipe-form__img_container"
-              onClick={this.uploadImage}
-            >
-            </section>
-            <section className="recipe-form__title-subform">
+      <section className="recipe-form-page">
+        <div className="recipe-form">
+          <div className="recipe-form__title-window">
+            <label className="recipe-form__img-upload">
+            <input
+              type="file"
+              className="recipe-form__img-input"
+              onChange={this.uploadImage}
+            />
+            {this.displayImage()}
+            </label>
+            <div className="recipe-form__title-subform">
               <label className="recipe-form__title-label">Title:
-                  <input type="text" value="fksakfsa" className="recipe-form__textinput" />
+                  <input type="text" 
+                  value="" 
+                  className="recipe-form__textinput"
+                  placeholder="Your Munchable titlesan"
+                  />
               </label>
               <label className="recipe-form__intro-label">Intro:<br />
                 <textarea
                   className="recipe-form__textarea">
                 </textarea>
                 <br />
-                <button className="te">Publish Munchable</button>
+                <button className="recipe-form__button">Publish Munchable</button>
               </label>
-            </section>
-          </section>
-        </section>
-      </main>
+            </div>
+          </div>
+        </div>
+        <button className="recipe-form__add-step-button">Add Step</button>
+      </section>
     );
   }
 }
