8d801866540be45836aa68e187943647eee7cd6d
Jonathan Abantao <jonabantao@gmail.com>
2018-02-05 11:50:11 -0800

    Add exact to the show container so that it wont interfere with the edit page

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -18,7 +18,7 @@ const App = () => (
       <AuthRoute path="/login" component={SessionFormContainer} />
       <AuthRoute path="/signup" component={SessionFormContainer} />
       <ProtectedRoute path="/recipes/new" component={RecipeFormContainer} />
-      <Route path="/recipes/:recipeId" component={RecipeItemDetailContainer} />
+      <Route exact path="/recipes/:recipeId" component={RecipeItemDetailContainer} />
       <Route path="/recipes/:recipeId/edit" component={RecipeFormContainer} />
       <Route component={NotFoundComponent} />
     </Switch>
