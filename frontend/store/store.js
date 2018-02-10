329351dff3e4a8e2bbe6ded87e5a3b472d76471a
Jonathan Abantao <jonabantao@gmail.com>
2018-02-09 16:01:27 -0800

    RIP redux logger

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -2,15 +2,9 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducer/root_reducer';
 
-const middlewares = [thunk];
-
-if (process.env.NODE_ENV !== 'production') {
-  const { logger } = require('redux-logger');
-  middlewares.push(logger);
-}
 
 const configureStore = (preloadedState = {}) => (
-  createStore(rootReducer, preloadedState, applyMiddleware(...middlewares))
+  createStore(rootReducer, preloadedState, applyMiddleware(thunk))
 );
 
 export default configureStore;
