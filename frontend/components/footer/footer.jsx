2a31612134d49b8f55d146b527c6e8965b6b7773
Jonathan Abantao <jonabantao@gmail.com>
2018-03-04 18:10:57 -0800

    Add Linkedin and Github icons

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <div className="footer">
-
-    </div>
+    <footer className="footer">
+      <div className="footer__container">
+        <a href="https://www.linkedin.com/in/jmabantao">
+          <i className="fab fa-linkedin footer__icon"></i>
+        </a>
+        <a href="https://github.com/jonabantao">
+          <i className="fab fa-github footer__icon"></i>
+        </a>
+      </div>
+    </footer>
   );
 };
 
