4d2b1c35d19d071eaefca9caf8d7860b8d51d98c
Jonathan Abantao <jonabantao@gmail.com>
2018-03-11 22:18:02 -0700

    Complete public profile view

--- frontend/components/user_profile/public_profile.jsx
+++ frontend/components/user_profile/public_profile.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
+import PublishedRecipeItem from './published_recipe_item';
 
-const PublicProfile = (props) => {
+const PublicProfile = ({ user, recipes }) => {
+  console.log(recipes);
+  if (!recipes.length) {
+    return (
+      <section className="profile-recipes-container">
+        <nav className="profile-recipes">
+          <h3 className="profile-recipes__header">
+          {user.username} has not published any munchables... yet.
+          </h3>
+        </nav>
+      </section>
+    );
+  }
+
+  const publishedRecipes = recipes.map(recipe => 
+    <PublishedRecipeItem
+      key={recipe.id}
+      recipeId={recipe.id}
+      title={recipe.title}
+      imageUrl={recipe.recipe_img_url}
+    />
+  );
+
   return (
     <section className="profile-recipes-container">
-
+      <nav className="profile-recipes">
+        <h3 className="profile-recipes__header">
+          {`${user.username}'s published munchables`.toUpperCase()}
+        </h3>
+        <ul className="profile-recipes__list">
+          {publishedRecipes}
+        </ul>
+      </nav>
     </section>
   );
 };
