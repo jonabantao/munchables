0db98cdc5a0f388ceef704413cdfea54e870cb98
Jonathan Abantao <jonabantao@gmail.com>
2018-04-10 17:09:21 -0700

    Update CSS loaders

--- frontend/components/loading_recipes.jsx
+++ frontend/components/loading_recipes.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 
-const LoadingRecipes = () => {
-  return (
-    <section className="loading">
-      <div className="la-ball-fussion la-dark la-2x">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-    </section>
-  );
-};
+const LoadingRecipes = () => (
+  <div className="sk-circle">
+    <div className="sk-circle1 sk-child" />
+    <div className="sk-circle2 sk-child" />
+    <div className="sk-circle3 sk-child" />
+    <div className="sk-circle4 sk-child" />
+    <div className="sk-circle5 sk-child" />
+    <div className="sk-circle6 sk-child" />
+    <div className="sk-circle7 sk-child" />
+    <div className="sk-circle8 sk-child" />
+    <div className="sk-circle9 sk-child" />
+    <div className="sk-circle10 sk-child" />
+    <div className="sk-circle11 sk-child" />
+    <div className="sk-circle12 sk-child" />
+  </div>
+);
 
 export default LoadingRecipes;
